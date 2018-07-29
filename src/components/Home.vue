c716d1e12b457206bc5b8a0d6e6d3788b300435c
Zenerboson <zenerboson@gmail.com>
2018-07-28 20:57:53 -0700

    fixed shadows

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -62,7 +62,7 @@ export default {
   justify-content: flex-start;
   align-items: center;
   flex-flow: column nowrap;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 }
 .productimage {
